2bc59b76397628f1c6312d42d25a4cc53b7e65fb
Mike Manilone <crtmike@gmail.com>
2012-04-15 01:09:49 +0800

    updated on `date`

---
+++ sync.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# 
+# The script moves these files to gtkqq.github.com
+# 
+
+# update gh-pages
+git add . 
+git commit -a -m 'updated on `date`'
+git push
+
+# update gtkqq.github.com
+cd /tmp/
+git clone git@github.com:gtkqq/gtkqq.github.com.git 
+cd - 
+cp * /tmp/gtkqq.github.com
+cd /tmp/gtkqq.github.com
+git add .
+git commit -a -m 'updated on `date`'
+git push
